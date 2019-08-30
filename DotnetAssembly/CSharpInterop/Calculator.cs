8c7753fba15919a0a6da368192cb71236b3c416c
Ace Olszowka <aolszowka@gmail.com>
2019-08-30 14:40:13 -0600

    Initial commit of the DotnetAssembly API Sample

---
+++ DotnetAssembly/CSharpInterop/Calculator.cs
@@ -0,0 +1,11 @@
+﻿namespace CSharpInterop
+{
+    public static class Calculator
+    {
+        public static int Add( int a, int b )
+        {
+            int result = a + b;
+            return result;
+        }
+    }
+}
