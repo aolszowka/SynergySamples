f4c241faf18f3a9c13496b2c91f92718e5cfe0b1
Steve Ives <steve.ives@synergex.com>
2020-06-08 15:32:50 -0700

    Use some WPF in calculator

--- DotnetAssembly/CSharpInterop/Calculator.cs
+++ DotnetAssembly/CSharpInterop/Calculator.cs
@@ -4,6 +4,7 @@
     {
         public static int Add( int a, int b )
         {
+            System.Windows.Interactivity.EventTrigger et = new System.Windows.Interactivity.EventTrigger();
             int result = a + b;
             return result;
         }
